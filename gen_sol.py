c89819047a011e7529f0940cc26d1369bd55c55f
Balagopal Komarath <baluks@gmail.com>
2009-10-20 16:07:55 +0530

    Added check for existence of outfile in gen_sol.py

--- gen_sol.py
+++ gen_sol.py
@@ -11,7 +11,8 @@ print ls()
 # Get all tex files
 tex_files = filter(lambda x: re.search("\.tex$", x), ls())
 print tex_files
-tex_files.remove(outfile) # Don't include any previous outputs
+if outfile in tex_files:
+	tex_files.remove(outfile) # Don't include any previous outputs
 
 # Remove the .tex suffix
 tex_files = map(lambda x: re.sub("\.tex", "", x), tex_files)
